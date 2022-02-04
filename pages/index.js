1baa83e17ed11080cb2567248b2d2b49e45e9ef4
Gilser <muellgabrielbeutl@gmail.com>
2022-02-04 11:47:00 +0100

    iframe

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+          <iframe src="https://coda.io/embed/RSrp0hxLN2/_suf-J?viewMode=embedplay" width=900 height=500 style="max-width: 100%;" allow="fullscreen"></iframe>
         </p>
       </main>
 
